b7ddb4cc9f884c41afd7bca31f8f94c15a1c10a8
Andres Vera <logros080@gmail.com>
2017-08-02 16:12:39 -0500

    Crear primera route :books

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  #Rutas
+  resources :books #Rutas libros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
