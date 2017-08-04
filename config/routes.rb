8087b915d3fa5bbd842aaa899e2b0e58e9601e6f
Andres Vera <logros080@gmail.com>
2017-08-03 19:09:16 -0500

    Implementa funcionalidad  de autencarse

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   #registro de usuarios
   resources :users, only: [:new, :create]
 
+  # Session de Usuarios
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   #Crud productos
   resources :books #Rutas libros
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
